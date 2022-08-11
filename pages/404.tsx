7ae8e408b2f2a9a5cbc33d9e0f4d7c5f3267cd92
Tomas <tomasborquez13@gmail.com>
2022-08-11 11:59:49 -0300

    test

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,7 +4,7 @@ import { Layout } from '../components/Layout/Layout';
 const error = () => {
   return (
     <Layout title={'Error | Tomas Borquez'}>
-      <h1>404 This page does not exist</h1>
+      <h1>404 This page does not exist 😡</h1>
     </Layout>
   );
 };
